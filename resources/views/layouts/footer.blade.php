f7758aa3a533742593e05a4a69a72be0a51661c1
hunter balling <muvimi0404@gmail.com>
2020-09-02 15:44:29 +0200

    Models, Controllers and preset

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,15 @@
+<footer class="footer">
+    <div class="container-fluid">
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="https://gmitech.com" target="blank" class="nav-link">
+                    {{ _('Great Minds Investments') }}
+                </a>
+            </li>            
+        </ul>
+        <div class="copyright">
+            &copy; {{ now()->year }} {{ _('made with') }} <i class="tim-icons icon-heart-2"></i> {{ _('by') }}
+            <a href="https://gmitech.com" target="_blank">{{ _('Great Mind Investments') }}</a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/navbars/sidebar.blade.php
@@ -0,0 +1,65 @@
+<div class="sidebar">
+    <div class="sidebar-wrapper">
+        <div class="logo">
+            <a href="#" class="simple-text logo-mini">{{ _('') }}</a>
+            <a href="#" class="simple-text logo-normal">{{ _('CUTASA') }}</a>
+        </div>
+        <ul class="nav">
+            <li @if ($pageSlug == 'dashboard') class="active " @endif>
+                <a href="{{ route('home') }}">
+                    <i class="tim-icons icon-chart-pie-36"></i>
+                    <p>{{ _('Dashboard') }}</p>
+                </a>
+            </li>
+            <li>
+                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
+                    <i class="fab fa-laravel" ></i>
+                    <span class="nav-link-text" >{{ __('Users') }}</span>
+                    <b class="caret mt-1"></b>
+                </a>
+
+                <div class="collapse show" id="laravel-examples">
+                    <ul class="nav pl-4">
+                        <li @if ($pageSlug == 'profile') class="active " @endif>
+                            <a href="{{ route('profile.edit')  }}">
+                                <i class="tim-icons icon-single-02"></i>
+                                <p>{{ _('User Profile') }}</p>
+                            </a>
+                        </li>
+                        <li @if ($pageSlug == 'users') class="active " @endif>
+                            <a href="{{ route('user.index')  }}">
+                                <i class="tim-icons icon-bullet-list-67"></i>
+                                <p>{{ _('User Management') }}</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+            <li @if ($pageSlug == 'icons') class="active " @endif>
+                <a href="{{ route('pages.icons') }}">
+                    <i class="tim-icons icon-atom"></i>
+                    <p>{{ _('Gallery') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'maps') class="active " @endif>
+                <a href="{{ route('pages.maps') }}">
+                    <i class="tim-icons icon-pin"></i>
+                    <p>{{ _('Sermons') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'notifications') class="active " @endif>
+                <a href="{{ route('pages.notifications') }}">
+                    <i class="tim-icons icon-bell-55"></i>
+                    <p>{{ _('Events') }}</p>
+                </a>
+            </li>
+            <li @if ($pageSlug == 'tables') class="active " @endif>
+                <a href="{{ route('pages.tables') }}">
+                    <i class="tim-icons icon-puzzle-10"></i>
+                    <p>{{ _('Donations') }}</p>
+                </a>
+            </li>
+            
+        </ul>
+    </div>
+</div>
